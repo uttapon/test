dd45ac78470f52f0c4f6922bf65f8f4f6d2acaea
uttapon <timzcomsci@gmail.com>
2015-06-28 11:21:08 +0700

    เพิ่มรายการข้อมูล
    หนา้เพิ่มรายการข้อมูล

---
+++ frm_insert.php
@@ -0,0 +1,17 @@
+﻿<html>
+	<head>
+		<title></title>
+	</head>
+
+	<body>
+		<form action="insert.php" name="frm_insert" method="POST">
+			<p>หมายเลขผลิตภัณฑ์:<input type="text" name="prod_id"></p>
+			<p>หมายเลขลูกค้า:<input type="text" name="vend_id"></p>
+			<p>ชื่อลูกค้า:<input type="text" name="prod_name"></p>
+			<p>ราคาสินค้า:<input type="text" name="prod_price"></p>
+			<p>รายระเอียดสินค้า:<input type="text" name="prod_desc"></p>
+			<button type"submit" name="btn_submit">เพิ่มชื่อ</button>
+			<a href="select.php">Back</a>
+		</form>
+	</body>
+</html>
